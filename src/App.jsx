759b418db64326c6766e128393b94ae760536056
wael latrech <wael.22@live.fr>
2023-09-14 19:41:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home"
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
